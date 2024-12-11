717ed4ab5a33ac40c5afbb089ec797e6e12e507d
shloakvatsyayan <shloak.vatsyayan@gmail.com>
2024-12-10 21:51:40 -0500

    merged mod-bot with this

--- commands/test.py
+++ commands/test.py
@@ -1,40 +1,13 @@
-from interactions import slash_command, slash_option, OptionType, SlashContext, Embed, SlashCommandChoice, Button, ButtonStyle, ActionRow, ComponentContext, StringSelectMenu
-from interactions.api.events import Component
-import time
+from interactions import slash_command, SlashContext, Embed
+
+from bot_instance import role_check
 from utils import colors
-from bot_instance import sr_role_check, bot
 
-@slash_command(
-    name="test",
-    description="",
-)
-@sr_role_check()
-async def test(ctx):
-    channel = ctx
-    button = Button(
-        custom_id="my_button_id",
-        style=ButtonStyle.GREEN,
-        label="Click Me",
-    )
-    message = await channel.send("Look a Button!", components=button)
 
-    # define the check
-    async def check(component: Component) -> bool:
-        if component.ctx.author.username.startswith("a"):
-            return True
-        else:
-            await component.ctx.send("Your name does not start with an 'a'!", ephemeral=True)
-
-    try:
-        # you need to pass the component you want to listen for here
-        # you can also pass an ActionRow, or a list of ActionRows. Then a press on any component in there will be listened for
-        used_component: Component = await bot.wait_for_component(components=button, check=check, timeout=30)
-
-    except TimeoutError:
-        print("Timed Out!")
-
-        button.disabled = True
-        await message.edit(components=button)
-
-    else:
-        await used_component.ctx.send("Your name starts with 'a'")
+@slash_command(name="test", description="Run predefined tests.")
+@role_check()
+async def handle_test_command(ctx: SlashContext):
+    test_start_msg = Embed(description="Running Tests...", color=colors.DiscordColors.GREEN)
+    msg = await ctx.send(embeds=test_start_msg)
+    test_complete_msg = Embed(description="Test Complete!", color=colors.DiscordColors.GREEN)
+    await msg.edit(embeds=test_complete_msg)
